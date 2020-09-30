9e5849b0590b9726ce7940cfeed52dc3dbb5ad9f
asadmansr <asadmansr@gmail.com>
2020-09-30 19:10:03 -0400

    Added: updated user detail page to show copy icon and save data to clipboard

--- lib/features/user_detail/presentation/widget/generate_user_detail.dart
+++ lib/features/user_detail/presentation/widget/generate_user_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:montageapp/core/constants/colors.dart' as Color;
 import 'package:montageapp/features/random_user/domain/entity/user.dart';
 
@@ -29,70 +30,150 @@ class GenerateUserDetailPage extends StatelessWidget {
           SizedBox(
             height: 8,
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Name',
-                  labelStyle: TextStyle(color: Color.whiteColor),
-                  enabledBorder: new UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color.formBorderColor))),
-              style: TextStyle(color: Color.whiteColor),
-              readOnly: true,
-              initialValue: this.user.name,
-            ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 24.0, right: 0.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Name',
+                        labelStyle: TextStyle(color: Color.whiteColor),
+                        enabledBorder: new UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Color.formBorderColor))),
+                    style: TextStyle(color: Color.whiteColor),
+                    readOnly: true,
+                    initialValue: this.user.name,
+                  ),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  _copyToClipboard(context, "Name", this.user.name);
+                },
+                child: Icon(
+                  Icons.content_copy,
+                  color: Color.whiteColor,
+                ),
+              )
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Color.whiteColor),
-                  enabledBorder: new UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color.formBorderColor))),
-              style: TextStyle(color: Color.whiteColor),
-              readOnly: true,
-              initialValue: this.user.email,
-            ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 24.0, right: 0.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(color: Color.whiteColor),
+                        enabledBorder: new UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Color.formBorderColor))),
+                    style: TextStyle(color: Color.whiteColor),
+                    readOnly: true,
+                    initialValue: this.user.email,
+                  ),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  _copyToClipboard(context, "Email", this.user.email);
+                },
+                child: Icon(
+                  Icons.content_copy,
+                  color: Color.whiteColor,
+                ),
+              )
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Username',
-                  labelStyle: TextStyle(color: Color.whiteColor),
-                  enabledBorder: new UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color.formBorderColor))),
-              style: TextStyle(color: Color.whiteColor),
-              readOnly: true,
-              initialValue: this.user.userName,
-            ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 24.0, right: 0.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Username',
+                        labelStyle: TextStyle(color: Color.whiteColor),
+                        enabledBorder: new UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Color.formBorderColor))),
+                    style: TextStyle(color: Color.whiteColor),
+                    readOnly: true,
+                    initialValue: this.user.userName,
+                  ),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  _copyToClipboard(context, "Username", this.user.userName);
+                },
+                child: Icon(
+                  Icons.content_copy,
+                  color: Color.whiteColor,
+                ),
+              )
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: TextStyle(color: Color.whiteColor),
-                  enabledBorder: new UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color.formBorderColor))),
-              style: TextStyle(color: Color.whiteColor),
-              readOnly: true,
-              initialValue: this.user.password,
-            ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 24.0, right: 0.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(color: Color.whiteColor),
+                        enabledBorder: new UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Color.formBorderColor))),
+                    style: TextStyle(color: Color.whiteColor),
+                    readOnly: true,
+                    initialValue: this.user.password,
+                  ),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  _copyToClipboard(context, "Password", this.user.password);
+                },
+                child: Icon(
+                  Icons.content_copy,
+                  color: Color.whiteColor,
+                ),
+              )
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Address',
-                  labelStyle: TextStyle(color: Color.whiteColor),
-                  enabledBorder: new UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color.formBorderColor))),
-              style: TextStyle(color: Color.whiteColor),
-              readOnly: true,
-              initialValue: this.user.address,
-            ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 24.0, right: 0.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Address',
+                        labelStyle: TextStyle(color: Color.whiteColor),
+                        enabledBorder: new UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Color.formBorderColor))),
+                    style: TextStyle(color: Color.whiteColor),
+                    readOnly: true,
+                    initialValue: this.user.address,
+                  ),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  _copyToClipboard(context, "Address", this.user.address);
+                },
+                child: Icon(
+                  Icons.content_copy,
+                  color: Color.whiteColor,
+                ),
+              )
+            ],
           ),
           SizedBox(
             height: 16,
@@ -100,5 +181,14 @@ class GenerateUserDetailPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void _copyToClipboard(BuildContext context, String title, String value) {
+    final snackBar = SnackBar(
+      content: Text("$title has been copied to clipboard"),
+      duration: Duration(seconds: 1),
+    );
+    Scaffold.of(context).showSnackBar(snackBar);
+    Clipboard.setData(new ClipboardData(text: value));
   }
 }
